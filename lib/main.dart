acc11bb539878bbb2a8cce7552d67e02bc9c0b5f
jithuraj <jithuigate@gmail.com>
2022-03-30 21:59:37 +0530

    improvements

--- lib/main.dart
+++ lib/main.dart
@@ -2,33 +2,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(MyApp());
+  runApp(myApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class myApp extends StatelessWidget {
+  const myApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Colors.cyan,
-      ),
-      home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        children: [Text("jihturaj"), Text("Bengalam")],
-      ),
-    );
+    return Container();
   }
 }
