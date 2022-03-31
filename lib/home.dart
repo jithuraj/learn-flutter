6a2901265018e07a4d338dba3c6aa9332d83ec48
jithuraj <jithuigate@gmail.com>
2022-03-31 21:46:20 +0530

    improvements

--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,54 @@
-import 'package:flutter/material.dart';
+import 'dart:html';
 
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+import 'package:flutter/material.dart';
+import 'package:myapp/screen_2.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  final _textController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    getSavedData(context);
+    return Scaffold(
+        body: Column(
+          children: [
+            TextFormField(
+              controller: _textController,
+            ),
+            ElevatedButton(onPressed: () {
+              SaveDataToStorage();
+            }, child: Text('Save value')),
+          ],
+        ));
+  }
+
+
+  Future<void> SaveDataToStorage() async {
+    // print(_textController.text);
+
+    final _sharedPrefence = await SharedPreferences.getInstance();
+    await _sharedPrefence.setString("saved_value", _textController.text);
+  }
+
+
+  Future<void> getSavedData(BuildContext context) async {
+    final _sharedPreference = await SharedPreferences.getInstance();
+
+    final _savedValue = _sharedPreference.getString("saved_value");
+    if (_savedValue != null) {
+      Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => Screen2()));
+    } else {
+
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:myapp/home.dart';
+import 'package:myapp/screen_2.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 main() {
   runApp(myApp());
@@ -10,6 +13,15 @@ class myApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Home(),
+      routes: {
+        'home': (ctx) => Home(),
+        'screen_2':(ctx)=>Screen2(),
+      },
+
+    );
   }
 }
+
+
